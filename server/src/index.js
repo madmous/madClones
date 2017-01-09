d8a9cd6a6e4bcb69591c557070d5d2b71f535f19
Moustapha A Diouf <mail@madmous.com>
2017-01-09 23:30:34 +0100

    Add docker

--- server/src/index.js
+++ server/src/index.js
@@ -23,25 +23,6 @@ const port = 3001;
 
 const app = express ();
 
-
-if (process.env.NODE_ENV === 'dev') {
-  var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-
-    // intercept OPTIONS method
-    if ('OPTIONS' == req.method) {
-      res.sendStatus(200);
-    }
-    else {
-      next();
-    }
-  };
-
-  app.use(allowCrossDomain);
-}
-
 app.use(passport.initialize());
 app.use(helmet());
 app.use(bodyParser.urlencoded({ extended: true }));
