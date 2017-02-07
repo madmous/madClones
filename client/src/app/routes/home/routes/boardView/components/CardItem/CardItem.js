14b37fd919198d893ebf92549a0c4f8e0eab242d
Moustapha A Diouf <mail@madmous.com>
2017-02-07 21:59:47 +0100

    Add PropTypes to CardItem component

--- client/src/app/routes/home/routes/boardView/components/CardItem/CardItem.js
+++ client/src/app/routes/home/routes/boardView/components/CardItem/CardItem.js
@@ -1,15 +1,22 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 
 import './CardItem.css';
 
+const propTypes = {
+  cardItemText: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired
+}
+
 export default class CardItem extends Component {
   render() {
-    const { connectDragSource, id } = this.props;
+    const { connectDragSource, cardItemText, id } = this.props;
 
     return connectDragSource(
       <div className="Card-Item" id={id}>
-        <p>{this.props.cardItemText}</p>
+        <p>{ cardItemText }</p>
       </div>
     );
   }
 }
+
+CardItem.propTypes = propTypes;
