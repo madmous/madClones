310b929ad3193b89cadc3804c6914539de751105
Moustapha A Diouf <mail@madmous.com>
2017-02-11 19:33:56 +0100

    Fix boardView styles

--- client/src/app/routes/home/routes/boardView/modules/boardView/boardView.js
+++ client/src/app/routes/home/routes/boardView/modules/boardView/boardView.js
@@ -1,5 +1,5 @@
-const OPEN_CREATE_CARD_FORM = 'OPEN_CREATE_CARD_FORM';
 const CLOSE_CREATE_CARD_FORM = 'CLOSE_CREATE_CARD_FORM';
+const OPEN_CREATE_CARD_FORM = 'OPEN_CREATE_CARD_FORM';
 
 const FOCUS_CREATE_CARD_FORM = 'FOCUS_CREATE_CARD_FORM';
 const BLUR_CREATE_CARD_FORM = 'BLUR_CREATE_CARD_FORM';
@@ -12,25 +12,25 @@ const initialState = {
 export function openCreateCardForm () {
   return {
     type: OPEN_CREATE_CARD_FORM
-  }
+  };
 }
 
 export function closeCreateCardForm () {
   return {
     type: CLOSE_CREATE_CARD_FORM
-  }
+  };
 }
 
 export function focusOnBoard() {
   return {
     type: FOCUS_CREATE_CARD_FORM,
-  }
+  };
 }
 
 export function blurOnBoard() {
   return {
     type: BLUR_CREATE_CARD_FORM,
-  }
+  };
 }
 
 export default function boardView(state = initialState, action) {
@@ -38,19 +38,19 @@ export default function boardView(state = initialState, action) {
     case OPEN_CREATE_CARD_FORM:
       return Object.assign({}, state, {
         isCreateCardFormOpen: true
-      })
+      });
     case CLOSE_CREATE_CARD_FORM:
       return Object.assign({}, state, {
         isCreateCardFormOpen: false
-      })
+      });
     case FOCUS_CREATE_CARD_FORM:
 			return Object.assign({}, state, {
 				isFocusOnCreateCardForm: true
-			})
+			});
 		case BLUR_CREATE_CARD_FORM:
 			return Object.assign({}, state, {
 				isFocusOnCreateCardForm: false
-			})
+			});
     default: return state;
   }
 }
