f1ba4daa6c728c7cb12f202e9a698bb5013266fb
Moustapha A Diouf <mail@madmous.com>
2017-02-12 20:02:16 +0100

    Refactor Board component unit tests

--- client/src/app/routes/home/components/Board/Board.spec.js
+++ client/src/app/routes/home/components/Board/Board.spec.js
@@ -4,7 +4,7 @@ import React from 'react';
 
 import Board from './Board';
 
-function setup() {
+const setupShallow = () => {
   const props = {
     displayBoardOptions: false,
     isStarredBoardItem: false,
@@ -17,60 +17,63 @@ function setup() {
 }
 
 describe('Board', () => {
-  it('should render h3 component', () => {
-    const wrapper = setup();
 
-    expect(wrapper.find('h3')).to.have.length(1);
+  describe('Board - render', () => {
+    it('should render h3 component', () => {
+      const wrapper = setupShallow();
+
+      expect(wrapper.find('h3')).to.have.length(1);
+    })
+
+    it('should render div components', () => {
+      const wrapper = setupShallow();
+
+      expect(wrapper.find('div')).to.have.length(3);
+    })
+
+    it('should render ul components', () => {
+      const wrapper = setupShallow();
+
+      expect(wrapper.find('ul')).to.have.length(1);
+    })
+
+    it('should render fontawesome component with user as name', () => {
+      const wrapper = setupShallow();
+
+      expect(wrapper.find('FontAwesome')).to.have.length(1);
+      expect(wrapper.find('FontAwesome').props().name).to.equal('user');
+    })
+
+    it('should render create new board component', () => {
+      const wrapper = setupShallow();
+
+      expect(wrapper.find('Connect(BoardItem)')).to.have.length(1);
+      expect(wrapper.find('Connect(BoardItem)').props().boardName).to.equal('Create new board...');
+    })
   })
 
-  it('should render div components', () => {
-    const wrapper = setup();
+  describe('Board - change props', () => {
+    it('should render fontawesome component with users as name', () => {
+      const wrapper = setupShallow();
 
-    expect(wrapper.find('div')).to.have.length(3);
-  })
+      wrapper.setProps({ displayBoardOptions: true });
+      expect(wrapper.find('FontAwesome')).to.have.length(1);
+      expect(wrapper.find('FontAwesome').props().name).to.equal('users');
+    })
 
-  it('should render ul components', () => {
-    const wrapper = setup();
+    it('should render fontawesome component with star as name', () => {
+      const wrapper = setupShallow();
 
-    expect(wrapper.find('ul')).to.have.length(1);
-  })
+      wrapper.setProps({ isStarredBoard: true });
+      expect(wrapper.find('FontAwesome')).to.have.length(1);
+      expect(wrapper.find('FontAwesome').props().name).to.equal('star');
+    })
 
-  it('should render fontawesome component with user as name', () => {
-    const wrapper = setup();
+    it('should not render create new board component', () => {
+      const wrapper = setupShallow();
 
-    expect(wrapper.find('FontAwesome')).to.have.length(1);
-    expect(wrapper.find('FontAwesome').props().name).to.equal('user');
-  })
-
-  it('should render create new board component', () => {
-    const wrapper = setup();
-
-    expect(wrapper.find('Connect(BoardItem)')).to.have.length(1);
-    expect(wrapper.find('Connect(BoardItem)').props().boardName).to.equal('Create new board...');
-  })
-})
-
-describe('Board - change props', () => {
-  it('should render fontawesome component with users as name', () => {
-    const wrapper = setup();
-
-    wrapper.setProps({ displayBoardOptions: true });
-    expect(wrapper.find('FontAwesome')).to.have.length(1);
-    expect(wrapper.find('FontAwesome').props().name).to.equal('users');
-  })
-
-  it('should render fontawesome component with star as name', () => {
-    const wrapper = setup();
-
-    wrapper.setProps({ isStarredBoard: true });
-    expect(wrapper.find('FontAwesome')).to.have.length(1);
-    expect(wrapper.find('FontAwesome').props().name).to.equal('star');
-  })
-
-  it('should not render create new board component', () => {
-    const wrapper = setup();
-
-    wrapper.setProps({ isStarredBoard: true });
-    expect(wrapper.find('Connect(BoardItem)')).to.have.length(0);
+      wrapper.setProps({ isStarredBoard: true });
+      expect(wrapper.find('Connect(BoardItem)')).to.have.length(0);
+    })
   })
 })
