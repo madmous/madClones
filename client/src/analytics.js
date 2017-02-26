9bd2bb91ce19fe9f6d3b59af67f49abbc1495a73
Moustapha A Diouf <mail@madmous.com>
2017-02-26 23:13:53 +0100

    Update google analytics

--- client/src/analytics.js
+++ client/src/analytics.js
@@ -1,6 +1,6 @@
 import ReactGA from 'react-ga'
 
-ReactGA.initialize('UA-91097110-1');
+ReactGA.initialize('UA-92655720-1');
 
 export default function logPageView() {
   ReactGA.set({ page: window.location.pathname });
