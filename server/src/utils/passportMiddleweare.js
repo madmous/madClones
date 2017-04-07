9398d4ca31ba60d131010ca672c82679f4f4aabf
Moustapha A Diouf <mail@madmous.com>
2017-04-07 19:13:12 +0200

    Switch back to callbacks

--- server/src/utils/passportMiddleweare.js
+++ server/src/utils/passportMiddleweare.js
@@ -49,15 +49,20 @@ opts.secretOrKey = secret;
 
 passport.use(new Strategy(opts, 
   function(jwt_payload, callback) {
-    userModel.findById(jwt_payload)
-      .then(user => {
-        if (user) {
+    const userId = jwt_payload;
+    
+    userModel.findById(userId, function(err, user) {
+      
+      if (err) {
+          return callback(err, false);
+      }
+
+      if (user) {
           return callback(null, user);
-        } else {
-          callback(null, { err: 'There is not a user for this token' });
-        }
-      })
-      .catch(error => callback(err, false));
+      } else {
+          return callback(null, { err: 'There is not a user for this token' }); 
+      }
+    });
   }
 ));
 
