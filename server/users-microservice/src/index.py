01674f272ac4d9e053bc3d8b7ca87ea8122f1fa9
Moustapha A Diouf <mail@madmous.com>
2017-04-25 23:56:16 +0200

    Map users micro service to port 3002

--- server/users-microservice/src/index.py
+++ server/users-microservice/src/index.py
@@ -127,4 +127,4 @@ api.add_resource(SignInController, '/signin')
 api.add_resource(SignUpController, '/signup')
 
 if __name__ == '__main__':
-    app.run(host = '0.0.0.0', port = 5000)
+    app.run(host = '0.0.0.0', port = 3002)
