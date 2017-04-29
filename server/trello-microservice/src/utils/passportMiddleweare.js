ba0f704a5b61d0a7beb6b7626a7943c4d59ca368
Moustapha A Diouf <mail@madmous.com>
2017-04-29 02:06:21 +0200

    Change the authentication process by using the users micro service

--- server/trello-microservice/src/utils/passportMiddleweare.js
+++ server/trello-microservice/src/utils/passportMiddleweare.js
@@ -1,87 +1,41 @@
-import { BasicStrategy } from 'passport-http';
-import passport from 'passport';
-import async from 'async';
-import jwt from 'jsonwebtoken';
+import fetch from 'node-fetch';
 
 import { userModel } from '../models/index';
-import { secret } from '../config/config';
 
-import {
-  ExtractJwt,
-  Strategy 
-} from 'passport-jwt';
+export const authenticatedWithToken = (req, res, next) => {
+  let csrf;
+  let jwt;
 
-passport.use(new BasicStrategy(
-  function(username, password, callback) {
+  if (req && req.cookies && req.headers){
+    csrf = req.headers.csrf;
+    jwt = req.cookies.jwt;
 
-    userModel.findOne({ name: username }, function (err, user) {
+    const opts = {
+      headers: {
+        csrf,
+        jwt
+      }
+    };
 
-      if (err) { 
-				return callback(err); 
-			}
-
-      if (!user) { 
-        return callback(null, { err: {usernameErr: 'There is not an account for this username' } }); 
-			}
-
-      user.arePasswordsMatching(password, function(err, isMatch) {
-        
-        if (err) { 
-          return callback(err); 
+    fetch('http://localhost:3002/signcheck', opts)
+      .then(res => {
+        if (res.status === 401) {
+          return null;
         }
 
-        if (!isMatch) { 
-          return callback(null, { err: { passwordErr: 'Invalid password' } }); 
+        return res.json();
+      })
+      .then(user => {
+        if (!user) {
+          return next();
+        } else {
+          return userModel.findOne({name: user.name})
+            .then(user => {
+              req.user = user;
+              
+              return next();
+          });
         }
-
-        let payload = {
-          'iss': 'users-microservice',
-          'userId': user._id,
-          'userName': user.name,
-          'userEmail': user.email
-        };
-
-        const token = jwt.sign(payload, secret)
-
-        return callback(null, { token });
-      });
-    });
+      })
   }
-));
-
-let opts = {};
-
-let cookieExtractor = function(req) {
-  let token = null;
-
-  if (req && req.cookies){
-    token = req.cookies['jwt'];
-  }
-
-  return token;
 };
-
-opts.jwtFromRequest = ExtractJwt.fromExtractors([cookieExtractor]);
-opts.secretOrKey = secret;
-
-passport.use(new Strategy(opts, 
-  function(jwt_payload, callback) {
-    const { userId } = jwt_payload;
-    
-    userModel.findById(userId, function(err, user) {
-      
-      if (err) {
-          return callback(err, false);
-      }
-
-      if (user) {
-          return callback(null, user);
-      } else {
-          return callback(null, { err: 'There is not a user for this token' }); 
-      }
-    });
-  }
-));
-
-export const authenticatedWithToken = passport.authenticate('jwt', { session : false });
-export const authenticatedWithBasic = passport.authenticate('basic', { session : false });
