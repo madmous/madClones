3d04d4145d3dd03046af380a0acbda7536981ec2
Moustapha A Diouf <mail@madmous.com>
2017-05-01 19:55:09 +0200

    Catch promises rejection

--- server/trello-microservice/src/utils/passportMiddleweare.js
+++ server/trello-microservice/src/utils/passportMiddleweare.js
@@ -2,6 +2,9 @@ import fetch from 'node-fetch';
 
 import { usersMicroserviceUrl } from '../config/config'
 import { userModel } from '../models/index';
+import getLogger from '../libs/winston';
+
+const log = getLogger(module);
 
 export const authenticatedWithToken = async (req, res, next) => {
   if (req && req.cookies && req.cookies.jwt && req.headers && req.headers.csrf) {
@@ -15,28 +18,37 @@ export const authenticatedWithToken = async (req, res, next) => {
       }
     };
 
-    let res = await fetch(`${usersMicroserviceUrl}/signcheck`, opts);
-    let data = await res.json();
+    try {
+      let res = await fetch(`${usersMicroserviceUrl}/signcheck`, opts);
+      let data = await res.json();
 
-    if (res.status === 401) {
-      return null;
-    }
+      if (res.status === 401) {
+        return null;
+      }
 
-    if (!data) {
-      next();
-    } else {
-      try {
-        let user = await userModel.findOne({name: data.name});
+      if (!data) {
+        next();
+      } else {
+        try {
+          let user = await userModel.findOne({name: data.name});
 
-        if (user) {
-          req.user = user;
+          if (user) {
+            req.user = user;
+            next();
+          }
+        } catch (error) {
+          log.error('User not found');
+          req.err = 'User not found';
           next();
         }
-      } catch (error) {
-        console.log(error)
       }
+    } catch (error) {
+      log.error('The sign check failed');
+      req.err = 'The sign check failed';
+      next();
     }
   } else {
+    log.error('Either the cookie or the token is missing');
     req.err = 'Either the cookie or the token is missing';
 
     next();
