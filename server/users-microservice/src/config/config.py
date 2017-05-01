b51074b5d103fb3d2b54b4cbc5ee297fb10c9d32
Moustapha A Diouf <mail@madmous.com>
2017-05-01 19:53:11 +0200

    Update the token secret

--- server/users-microservice/src/config/config.py
+++ server/users-microservice/src/config/config.py
@@ -1,6 +1,6 @@
 import os
 
-jwtSecret = '_202ph=oob#c*a&_bvrb24za#v=%$70(+ycu_gz+od96m!_dmx'
+jwtSecret = '80AiE6Lul5tzO1Lm1Zwd1ayamScTspATlTNnB1eRsRSCldnyd8L02lYZJ2cKtZ4igjsPErQeUbXNRXOW5QpQ1mUMD23I4IWMBzYWBDmr9xc38kXFyNaBNm2W31kitaeI6PHdkcUN7KC89Put2jdri6ED-xZG5JAbqEg4GDEOwSRCnIcfWtO_nskls0qlPN5hWfQTzFZz-8veJowwKjrF_2xHV-0LDnAX5nX3qRbzmwFDIPvCDE0Zl60Ov8_IB2gN28tP-epouzdeZFd45uaB3MrDSmpuHRm-RU_vsU6tPLTLmCYqKxxB2HCZQ37OaaZMOyVQiN6jCDG293iRlsnJoA'
 
 global trelloMicroserviceUrl
 
