6b22520db58affc8bff4dd3176f4658c510a096b
Moustapha A Diouf <mail@madmous.com>
2017-05-01 02:33:08 +0200

    Use default port 3001

--- server/users-microservice/src/config/config.py
+++ server/users-microservice/src/config/config.py
@@ -1,1 +1,10 @@
-jwtSecret = 'jwt secret for users microservice access token'
+import os
+
+jwtSecret = '_202ph=oob#c*a&_bvrb24za#v=%$70(+ycu_gz+od96m!_dmx'
+
+global trelloMicroserviceUrl
+
+if 'FLASK_ENV' in os.environ.keys() and os.environ['FLASK_ENV'] == 'prod':
+    trelloMicroserviceUrl = 'http://trellomicroservice:3001'
+else:
+    trelloMicroserviceUrl = 'http://127.0.0.1:3001'
