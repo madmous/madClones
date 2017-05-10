7b22ce6a83ab57259e3d2dea827a1f734938d909
Moustapha A Diouf <mail@madmous.com>
2017-05-10 20:54:03 +0200

    Update development url

--- client/trello/src/utils/url.js
+++ client/trello/src/utils/url.js
@@ -2,11 +2,11 @@ let usersUrlToFetch;
 let urlToFetch;
 
 if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test') {
-  urlToFetch = 'http://localhost:3001/';
-  usersUrlToFetch = 'http://localhost:3002/'
+  urlToFetch = 'http://192.168.99.100:31388/';
+  usersUrlToFetch = 'http://192.168.99.100:31692/';
 } else {
   urlToFetch = 'http://ec2-52-56-95-197.eu-west-2.compute.amazonaws.com/';
 }
 
-export const usersUrl = usersUrlToFetch
+export const usersUrl = usersUrlToFetch;
 export const url = urlToFetch;
