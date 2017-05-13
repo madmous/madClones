ffb77d9c599ca316033e6cacbb697c6695afa529
Moustapha A Diouf <mail@madmous.com>
2017-05-13 11:20:05 +0200

    Test script exit code

--- .travis/build.sh
+++ .travis/build.sh
@@ -4,11 +4,4 @@ docker logs -f madclones_trello-microservice_1
 
 docker wait madclones_trello-microservice_1
 
-if [ "$?" -eq 1 ]
-then 
-  echo "Error"
-  exit 1
-else 
-  echo "No error"
-  exit 0
-fi
+exit $?
