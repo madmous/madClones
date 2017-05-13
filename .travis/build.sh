fe4ad1fb488795dfd9e7608399c829990cabdbc1
Moustapha A Diouf <mail@madmous.com>
2017-05-13 12:41:33 +0200

    Test travis build

--- .travis/build.sh
+++ .travis/build.sh
@@ -2,15 +2,11 @@
 
 docker logs -f madclones_trello-microservice_1
 
-rc = docker wait madclones_trello-microservice_1
+var=$(docker wait madclones_trello-microservice_1)
 
-if [ rc -eq 0 ]
+if [ $var -eq 0 ]
 then
-  echo $?
-  echo "Success"
   exit 0
 else
-  echo $?
-  echo "Failure"
   exit 1
 fi
