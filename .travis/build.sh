d0bfef35d1a865313d59329ce995cc584a0711c4
Moustapha A Diouf <mail@madmous.com>
2017-05-13 11:14:35 +0200

    Test if else condition

--- .travis/build.sh
+++ .travis/build.sh
@@ -4,7 +4,11 @@ docker logs -f madclones_trello-microservice_1
 
 docker wait madclones_trello-microservice_1
 
-if [ "$?" -ne 0 ]
+if [ "$?" -eq 1 ]
 then 
+  echo "Error"
   exit 1
+else 
+  echo "No error"
+  exit 0
 fi
