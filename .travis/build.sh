fcb88a096e584bc5838a79d75ff812485b47aa16
Moustapha A Diouf <mail@madmous.com>
2017-05-18 18:48:40 +0200

    Use on file for all the CI build

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,4 +1,29 @@
 #!/bin/bash
 
-docker logs -f madclones_trello-microservice_1
-[ $(docker wait madclones_trello-microservice_1) = 0 ]
+before() {
+  docker-compose -f dc-trello.test.yml up -d
+}
+
+main() {
+  docker logs -f madclones_trello-microservice_1
+  [ $(docker wait madclones_trello-microservice_1) = 0 ]
+}
+
+after() {
+  docker-compose down trellomicroservice_trello-microservice_1
+  docker-compose down trellomicroservice_mongo_1 
+}
+
+case "$1" in
+  before)
+      before
+      ;;
+    
+  main)
+      main
+      ;;
+    
+  after)
+      after
+      ;;
+esac
