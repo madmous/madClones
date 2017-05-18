541560e6b624119218729d70889197eddd184ca5
Moustapha A Diouf <mail@madmous.com>
2017-05-18 21:07:30 +0200

    Add logs to the test server

--- server/trello-microservice/test/index.js
+++ server/trello-microservice/test/index.js
@@ -2,8 +2,10 @@ import mongoose from 'mongoose';
 import sinon from 'sinon';
 
 import { dbTest } from '../src/config/database';
+import getLogger from '../src/libs/winston';
+import { port } from '../src/config/config';
 
-let isConnected;
+const log = getLogger(module);
 
 const prepareServer = (user, done) => {
   let passportMiddleweare = require('../src/utils/passportMiddleweare');
@@ -19,7 +21,9 @@ const prepareServer = (user, done) => {
   delete require.cache[require.resolve('../src/app')];
   let app = require('../src/app').default;
 
-  let server = app.listen(30001, (err) => {
+  let server = app.listen(port, (err) => {
+    log.info(`Test server is listening on ${port}`);
+
     if (mongoose.connection.readyState === 0) {
       dbTest.connect();
     }
