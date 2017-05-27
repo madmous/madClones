2b3e0d9b3a745d7f12fef80f6b587b1e442e9aaf
Moustapha A Diouf <mail@madmous.com>
2017-05-27 18:33:24 +0200

    Update trello client urls

--- client/trello/src/utils/url.js
+++ client/trello/src/utils/url.js
@@ -1,26 +1,22 @@
 export const usersUrl = (() => {
-  let users = '';
+  let users = 'http://localhost:3002/users/';
 
   if (process.env.REACT_APP_ENV === 'kuber-dev') {
-    users =  'http://madmous.madclones.server/users/';
-  } else if (process.env.REACT_APP_ENV === 'dev' || process.env.REACT_APP_ENV === 'docker-dev' || process.env.REACT_APP_ENV === 'docker-tst') {
-    users = 'http://localhost:3002/users/';
+    users =  'http://madclones.local/users/';
   } else {
-    users = 'http://madmous-madclones-server.com/users/';
+    users = 'http://madmous-madclones-server.com/users'
   }
 
   return users;
 })();
 
 export const trelloUrl = (() => {
-  let trello = '';
+  let trello = 'http://localhost:3001/trello/';
 
   if (process.env.REACT_APP_ENV === 'kuber-dev') {
-    trello =  'http://madmous.madclones.server/trello/';
-  } else if (process.env.REACT_APP_ENV === 'dev' || process.env.REACT_APP_ENV === 'docker-dev' || process.env.REACT_APP_ENV === 'docker-tst') {
-    trello = 'http://localhost:3001/trello/';
+    trello =  'http://madclones.local/trello/';
   } else {
-    trello = 'http://madmous-madclones-server.com/trello/';
+    trello = 'http://madmous-madclones-server.com/trello/'
   }
 
   return trello;
