9755399dfaf6dc3b06b2ef63fee0a2953179e7ba
Moustapha A Diouf <mail@madmous.com>
2017-05-27 18:22:52 +0200

    Update madclones server host name

--- client/trello/src/utils/url.js
+++ client/trello/src/utils/url.js
@@ -2,11 +2,11 @@ export const usersUrl = (() => {
   let users = '';
 
   if (process.env.REACT_APP_ENV === 'kuber-dev') {
-    users =  'http://madclones.local/users/';
+    users =  'http://madmous.madclones.server/users/';
   } else if (process.env.REACT_APP_ENV === 'dev' || process.env.REACT_APP_ENV === 'docker-dev' || process.env.REACT_APP_ENV === 'docker-tst') {
     users = 'http://localhost:3002/users/';
   } else {
-    users = 'http://35.187.64.216:3002/users/';
+    users = 'http://madmous-madclones-server.com/users/';
   }
 
   return users;
@@ -16,11 +16,11 @@ export const trelloUrl = (() => {
   let trello = '';
 
   if (process.env.REACT_APP_ENV === 'kuber-dev') {
-    trello =  'http://madclones.local/trello/';
+    trello =  'http://madmous.madclones.server/trello/';
   } else if (process.env.REACT_APP_ENV === 'dev' || process.env.REACT_APP_ENV === 'docker-dev' || process.env.REACT_APP_ENV === 'docker-tst') {
     trello = 'http://localhost:3001/trello/';
   } else {
-    trello = 'http://104.199.100.179:3001/trello/';
+    trello = 'http://madmous-madclones-server.com/trello/';
   }
 
   return trello;
