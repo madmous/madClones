8c08b6ea12ed915a9a83e02bd67d3f7020899455
Moustapha A Diouf <mail@madmous.com>
2017-06-07 18:50:16 +0200

    Fix trello client urls

--- client/trello/src/utils/url.js
+++ client/trello/src/utils/url.js
@@ -1,7 +1,9 @@
 export const usersUrl = (() => {
-  let users = 'http://localhost:3002/';
+  let users = '';
 
-  if (process.env.REACT_APP_ENV === 'kuber-dev') {
+  if (process.env.REACT_APP_ENV === 'dev' || process.env.REACT_APP_ENV === 'docker-dev') { 
+    users = 'http://localhost:3002/';
+  } else if (process.env.REACT_APP_ENV === 'kuber-dev') {
     users = 'http://madclones.local/users/';
   } else if (process.env.REACT_APP_ENV === 'prod')  {
     users = 'http://104.154.41.57:3002/'
@@ -11,9 +13,11 @@ export const usersUrl = (() => {
 })();
 
 export const trelloUrl = (() => {
-  let trello = 'http://localhost:3001/';
+  let trello = '';
 
-  if (process.env.REACT_APP_ENV === 'kuber-dev') {
+  if (process.env.REACT_APP_ENV === 'dev' || process.env.REACT_APP_ENV === 'docker-dev') { 
+    trello = 'http://localhost:3001/';
+  } else if (process.env.REACT_APP_ENV === 'kuber-dev') {
     trello = 'http://madclones.local/trello/';
   } else if (process.env.REACT_APP_ENV === 'prod') {
     trello = 'http://130.211.201.76:3001/'
